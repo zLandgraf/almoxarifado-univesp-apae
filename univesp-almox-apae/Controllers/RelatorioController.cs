0f01aac557a13a6f49c8a74dd5d4d00f2b664c7f
Alexandre Henrique Landgraf <alexandrehlandgraf@gmail.com>
2023-05-09 14:41:23 -0300

    adicionando page de relatório

---
+++ univesp-almox-apae/Controllers/RelatorioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace univesp.almox.apae.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
